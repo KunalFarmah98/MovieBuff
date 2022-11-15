18e5b4dca93c43c7ac06885ed72d6bbe6c78d8cc
Kunal Farmah <kunalfarmah98@gmail.com>
2022-11-15 23:45:48 +0530

    fixed string

--- app/src/main/java/com/kunalfarmah/moviebuff/util/Constants.kt
+++ app/src/main/java/com/kunalfarmah/moviebuff/util/Constants.kt
@@ -16,7 +16,7 @@ class Constants {
 
      object SortOrder {
          val POPULAIRTY = "Popularity"
-         val RELEASE_DATE = "ReleaseData"
+         val RELEASE_DATE = "ReleaseDate"
          val RATING = "Rating"
     }
 
